0841bc5603bcbc44815c6a4d909801a093487265
Alex Goretoy <alex@goretoy.com>
2024-08-23 11:34:39 +0000

    Updates

---
+++ _next/static/chunks/app/en/page-0a737cb121883734.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[479],{8220:function(n,e,u){Promise.resolve().then(u.bind(u,9486))},9486:function(n,e,u){"use strict";u.r(e),u.d(e,{default:function(){return r}});var t=u(1138);function r(){return(0,t.Z)({params:{locale:"en"}})}}},function(n){n.O(0,[532,138,971,69,744],function(){return n(n.s=8220)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/ru/page-7257aa40cfc2198a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[487],{6776:function(n,u,e){Promise.resolve().then(e.bind(e,4317))},4317:function(n,u,e){"use strict";e.r(u),e.d(u,{default:function(){return t}});var r=e(1138);function t(){return(0,r.Z)({params:{locale:"ru"}})}}},function(n){n.O(0,[532,138,971,69,744],function(){return n(n.s=6776)}),_N_E=n.O()}]);
